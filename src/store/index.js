79cd683fb91ef8523d7f2fac69a63babdc3a3e00
nikolaychirkov <nikolay4irkov@gmail.com>
2021-04-17 18:06:06 +0300

    created notification module for showing errors if failed fetching event/events and success message after creating

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import Vuex from "vuex";
 
 import * as user from "./modules/user.js";
 import * as event from "./modules/event";
+import * as notification from "./modules/notification";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     user,
     event,
+    notification,
   },
   state: {
     categories: [
--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -24,12 +24,26 @@ export const mutations = {
 };
 
 export const actions = {
-  createEvent({ commit }, event) {
-    return EventService.postEvent(event).then(() => {
-      commit("ADD_EVENT", event);
-    });
+  createEvent({ commit, dispatch }, event) {
+    return EventService.postEvent(event)
+      .then(() => {
+        commit("ADD_EVENT", event);
+        const notification = {
+          type: "success",
+          message: "Your event has been created!",
+        };
+        dispatch("notification/add", notification, { root: true });
+      })
+      .catch((error) => {
+        const notification = {
+          type: "error",
+          message: "There was a problem creating event: " + error.message,
+        };
+        dispatch("notification/add", notification, { root: true });
+        throw error;
+      });
   },
-  fetchEvents({ commit }, { page, perPage }) {
+  fetchEvents({ commit, dispatch }, { page, perPage }) {
     EventService.getEvents(page, perPage)
       .then((response) => {
         commit("SET_EVENTS", response.data);
@@ -39,10 +53,14 @@ export const actions = {
         );
       })
       .catch((error) => {
-        console.log("There was an error:" + error.response);
+        const notification = {
+          type: "error",
+          message: "There was a problem fetching events: " + error.message,
+        };
+        dispatch("notification/add", notification, { root: true });
       });
   },
-  fetchEvent({ commit, getters }, id) {
+  fetchEvent({ commit, getters, dispatch }, id) {
     var event = getters.getEventById(id);
 
     if (event) {
@@ -53,7 +71,11 @@ export const actions = {
           commit("SET_EVENT", response.data);
         })
         .catch((error) => {
-          console.log("There was an error:" + error.response);
+          const notification = {
+            type: "error",
+            message: "There was a problem fetching event: " + error.message,
+          };
+          dispatch("notification/add", notification, { root: true });
         });
     }
   },
